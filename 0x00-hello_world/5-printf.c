3424ea677502627b8a873b02b41e7d1816275fc4
khalid99 <yelkadiri39@gmail>
2024-06-30 05:32:45 -0700

    program that prints exactly Programming is like building a multilingual puzzle followed by a new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main print with proper grammar, but the outcome is a piece of art,
- * return 0 if exited properly , non-zero otherwise
+ * main- print with proper grammar, but the outcome is a piece of art,
+ * Return: 0 if exited properly , non-zero otherwise
  */
+
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
+	return (0);
 }
