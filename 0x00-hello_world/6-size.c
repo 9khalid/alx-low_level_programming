5b8c9fbf69480f0843604821b3a652f946d8a3d5
khalid99 <yelkadiri39@gmail>
2024-07-09 04:19:18 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
 int main(void)
 {
-	printf("Size of a char:%c\n", mytext);
-	printf("Size of an int:%d\n", mytext1);
-	printf("Size of a long int: 4 byte(s)\n");
-	printf("Size of a long long int: 8 byte(s)\n");
-	printf("Size of a float: 4 byte(s)\n");
-	printf("Size of a long int: 8 byte(s)\n");
+	printf("Size of a char: %s byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
